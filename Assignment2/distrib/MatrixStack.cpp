5c1e624ff081d513897509a428458df4d207e03a
NamiNaziri <naminaziri1@gmail.com>
2021-03-27 03:14:36 +0430

    Added implementation of Matrix Stack class.

--- Assignment2/distrib/MatrixStack.cpp
+++ Assignment2/distrib/MatrixStack.cpp
@@ -3,19 +3,23 @@
 MatrixStack::MatrixStack()
 {
 	// Initialize the matrix stack with the identity matrix.
-	
+	m_matrices.push_back(Matrix4f::identity());
 }
 
 void MatrixStack::clear()
 {
 	// Revert to just containing the identity matrix.
-
+	while (m_matrices.size() != size_t(1))
+	{
+		m_matrices.pop_back();
+	}
 }
 
 Matrix4f MatrixStack::top()
 {
 	// Return the top of the stack
 	
+	return (m_matrices.back());
 	
 }
 
@@ -25,9 +29,11 @@ void MatrixStack::push( const Matrix4f& m )
 	// Push m onto the stack.
 	// Your stack should have OpenGL semantics:
 	// the new top should be the old top multiplied by m
+	m_matrices.push_back(top() * m);
 }
 
 void MatrixStack::pop()
 {
+	m_matrices.pop_back();
 	// Remove the top element from the stack
 }
